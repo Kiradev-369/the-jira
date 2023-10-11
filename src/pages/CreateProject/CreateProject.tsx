a710ef8a0b750bc8e2192d2f581bce49fa90b91c
Kiradev <kira.hoangphong@gmail.com>
2023-10-11 20:27:36 +0700

    add

--- src/pages/CreateProject/CreateProject.tsx
+++ src/pages/CreateProject/CreateProject.tsx
@@ -1,120 +1,120 @@
-import React, { useRef, useEffect, useState } from "react";
-import css from "./CreateProject.module.scss";
-import { Editor } from "@tinymce/tinymce-react";
-import { useSelector, useDispatch } from "react-redux";
-import { projectCategory } from "../../services/projectCaregory.service";
-import { setCategoryProject } from "../../redux/slices/ProjectCategory.slice";
+// import React, { useRef, useEffect, useState } from "react";
+// import css from "./CreateProject.module.scss";
+// import { Editor } from "@tinymce/tinymce-react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { projectCategory } from "../../services/projectCaregory.service";
+// import { setCategoryProject } from "../../redux/slices/ProjectCategory.slice";
 
-function CreateProject() {
-  const editorRef = useRef<any>(null);
-  const [projectData, setProjectData] = useState({
-    projectName: "",
-    description: "",
-    categoryId: "",
-  });
+// function CreateProject() {
+//   const editorRef = useRef<any>(null);
+//   const [projectData, setProjectData] = useState({
+//     projectName: "",
+//     description: "",
+//     categoryId: "",
+//   });
 
-  const arrProjectCategory = useSelector(
-    (state: any) => state.projectCategoryReducer.projectCategory
-  );
-  const dispatch = useDispatch();
+//   const arrProjectCategory = useSelector(
+//     (state: any) => state.projectCategoryReducer.projectCategory
+//   );
+//   const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async () => {
-      const resp = await projectCategory();
-      dispatch(setCategoryProject(resp.content));
-    })();
-  }, []);
+//   useEffect(() => {
+//     (async () => {
+//       const resp = await projectCategory();
+//       dispatch(setCategoryProject(resp.content));
+//     })();
+//   }, []);
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setProjectData({
-      ...projectData,
-      [name]: value,
-    });
-  };
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setProjectData({
+//       ...projectData,
+//       [name]: value,
+//     });
+//   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
 
-    // Gửi dữ liệu dự án lên máy chủ để tạo dự án mới
-    try {
-      const response = await fetch("/api/createProject", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(projectData),
-      });
+//     // Gửi dữ liệu dự án lên máy chủ để tạo dự án mới
+//     try {
+//       const response = await fetch("/api/createProject", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(projectData),
+//       });
 
-      if (response.ok) {
-        const data = await response.json();
-        // Xử lý phản hồi từ máy chủ (có thể hiển thị thông báo hoặc điều hướng đến trang khác)
-        console.log("Dự án đã được tạo:", data);
-      } else {
-        console.error("Lỗi khi tạo dự án");
-      }
-    } catch (error) {
-      console.error("Lỗi khi gửi yêu cầu:", error);
-    }
-  };
+//       if (response.ok) {
+//         const data = await response.json();
+//         // Xử lý phản hồi từ máy chủ (có thể hiển thị thông báo hoặc điều hướng đến trang khác)
+//         console.log("Dự án đã được tạo:", data);
+//       } else {
+//         console.error("Lỗi khi tạo dự án");
+//       }
+//     } catch (error) {
+//       console.error("Lỗi khi gửi yêu cầu:", error);
+//     }
+//   };
 
-  return (
-    <div className={css["main"]}>
-      <h3>Create Project</h3>
-      <form className="content" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <p>Name</p>
-          <input
-            className="form-control"
-            name="projectName"
-            value={projectData.projectName}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <p>Description</p>
-          <Editor
-            onInit={(evt, editor) => (editorRef.current = editor)}
-            initialValue="<p></p>"
-            init={{
-              height: 500,
-              menubar: false,
-              plugins: [
-                "advlist autolink lists link image charmap print preview anchor",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table paste code help wordcount",
-              ],
-              toolbar:
-                "undo redo | formatselect | " +
-                "bold italic backcolor | alignleft aligncenter " +
-                "alignright alignjustify | bullist numlist outdent indent | " +
-                "removeformat | help",
-              content_style:
-                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-            }}
-          />
-        </div>
-        <div className="form-group">
-          <select
-            style={{ fontSize: 17 }}
-            name="categoryId"
-            className="form-control"
-            value={projectData.categoryId}
-            onChange={handleInputChange}
-          >
-            {arrProjectCategory.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <button style={{ fontSize: 17 }} className="btn btn-info" type="submit">
-          Create Project
-        </button>
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div className={css["main"]}>
+//       <h3>Create Project</h3>
+//       <form className="content" onSubmit={handleSubmit}>
+//         <div className="form-group">
+//           <p>Name</p>
+//           <input
+//             className="form-control"
+//             name="projectName"
+//             value={projectData.projectName}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         <div className="form-group">
+//           <p>Description</p>
+//           <Editor
+//             onInit={(evt, editor) => (editorRef.current = editor)}
+//             initialValue="<p></p>"
+//             init={{
+//               height: 500,
+//               menubar: false,
+//               plugins: [
+//                 "advlist autolink lists link image charmap print preview anchor",
+//                 "searchreplace visualblocks code fullscreen",
+//                 "insertdatetime media table paste code help wordcount",
+//               ],
+//               toolbar:
+//                 "undo redo | formatselect | " +
+//                 "bold italic backcolor | alignleft aligncenter " +
+//                 "alignright alignjustify | bullist numlist outdent indent | " +
+//                 "removeformat | help",
+//               content_style:
+//                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+//             }}
+//           />
+//         </div>
+//         <div className="form-group">
+//           <select
+//             style={{ fontSize: 17 }}
+//             name="categoryId"
+//             className="form-control"
+//             value={projectData.categoryId}
+//             onChange={handleInputChange}
+//           >
+//             {arrProjectCategory.map((category) => (
+//               <option key={category.id} value={category.id}>
+//                 {category.name}
+//               </option>
+//             ))}
+//           </select>
+//         </div>
+//         <button style={{ fontSize: 17 }} className="btn btn-info" type="submit">
+//           Create Project
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
 
-export default CreateProject;
+// export default CreateProject;
